3c39abf98b818ba27da4a148ac976e0b8ad5f5d0
Deadman407 <dead.407.man@gmail.com>
2021-12-25 12:57:43 -0800

    Added Download functionality

--- opencvEnv/src/opencv_proj/urls.py
+++ opencvEnv/src/opencv_proj/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from uploads.views import uploadImage
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', uploadImage, name='upload_image'),
+    url(r'^download/(?P<path>.*)$',serve,{'document.root':settings.MEDIA_ROOT}),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- opencvEnv/src/uploads/views.py
+++ opencvEnv/src/uploads/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import UploadForm
 from .models import Upload
+
+import time
 
 def uploadImage(request):
     form = UploadForm()
@@ -16,7 +19,19 @@ def uploadImage(request):
     if Upload.objects.all().count() > 0:
         upload = Upload.objects.latest('created')
         context = {'form': form, 'upload': upload}
+
+        time.sleep(10)
         upload.delete()
 
     return render(request, 'upload-form.html', context)
+
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/upload")
+            response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
+            return response
+
+    raise Http404
 
