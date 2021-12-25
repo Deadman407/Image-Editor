d699544c4e766b8c77229abb1b1b1b67736a5d30
Deadman407 <dead.407.man@gmail.com>
2021-12-25 07:44:25 -0800

    Added frontend

--- opencvEnv/src/opencv_proj/urls.py
+++ opencvEnv/src/opencv_proj/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from uploads.views import uploadImage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', uploadImage, name='upload_image'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- opencvEnv/src/uploads/views.py
+++ opencvEnv/src/uploads/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UploadForm
+from .models import Upload
 
-# Create your views here.
+def uploadImage(request):
+    form = UploadForm()
+
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_image')
+    
+    if Upload.objects.all().count() > 0:
+        upload = Upload.objects.latest('created')
+        context = {'form': form, 'upload': upload}
+        upload.delete()
+
+    return render(request, 'upload-form.html', context)
+
